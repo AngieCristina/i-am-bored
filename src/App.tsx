82af71b2b66d2d4baa830943e1e4214240545def
AngieCristina <angiecristinagonzalessara@gmail.com>
2023-01-26 00:04:57 -0500

    add funcionality call data through input

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,14 @@ import {
   Code,
   Grid,
   theme,
+  Input,
+  Card,
+  CardHeader,
+  Heading,
+  CardBody,
+  CardFooter,
+  Stack,
+  Image,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Logo } from "./Logo";
@@ -15,59 +23,73 @@ import { useEffect, useState } from "react";
 
 interface Activity {
   activity: string;
-  accessibility: number;
-  type: string;
   participants: number;
-  price: number;
-  link: string;
-  key: string;
 }
 
 function App() {
-  const [activities, setActivities] = useState<Activity>();
-  const [loading, setLoading] = useState(true);
+  const [activity, setActivity] = useState<Activity>();
+  const [loading, setLoading] = useState(false);
   const [participants, setParticipants] = useState("");
 
   const fetchActivityData = () => {
-    fetch(`https://www.boredapi.com/api/activity?participants=${participants}`)
+    setLoading(true);
+    fetch(`http://www.boredapi.com/api/activity?participants=${participants}`)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
         setLoading(false);
-        setActivities(data);
+        setActivity(data);
       });
   };
   useEffect(() => {
-    fetchActivityData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    if (participants) {
+      fetchActivityData();
+      setLoading(false);
+    }
   }, [participants]);
 
   if (loading) {
     return <div>loading</div>;
   }
 
-  if (!activities) return null;
-
   return (
-    <div>
-      {Object.keys(activities).map((key) => (
-        <li key={key}>
-          <b>{key}</b> : {activities[key as keyof Activity]}
-        </li>
-      ))}
-      <label>
-        Participants:
-        <input
-          type="number"
-          value={participants}
-          onChange={(e) => setParticipants(e.target.value)}
-          max={5}
-          required={false}
-          placeholder="max. 6 participants "
-        />
-      </label>
-    </div>
+    <Box display="flex">
+      <Card
+        overflow="hidden"
+        border="1px solid #000"
+        margin="auto"
+      >
+        <Stack>
+          <CardBody>
+            {activity && (
+              <Box>
+                <Heading size="xs">{activity.activity}</Heading>
+                <Text>boring people: {activity.participants}</Text>
+              </Box>
+            )}
+          </CardBody>
+
+          <Input
+            type="number"
+            value={participants}
+            onChange={(e) => {
+              setParticipants(e.target.value);
+            }}
+            max={5}
+            required={false}
+            placeholder="how many boring people?"
+          />
+
+          <CardFooter>
+            <Image
+              src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/d6649a62874689.5a9ea800b1d58.jpg"
+              alt="bored"
+            />
+          </CardFooter>
+        </Stack>
+      </Card>
+    </Box>
   );
 }
 
